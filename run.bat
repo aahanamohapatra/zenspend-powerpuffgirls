d524d8635f751ce25ba7bdd23d5956f3276c6fd0
aahanamohapatra <aahanamohapatra29@gmail.com>
2026-09-19 01:09:36 +0530

    Delete run.bat

--- run.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo ====================================================
-echo Starting ZenSpend Local Server...
-echo ====================================================
-pip install -r requirements.txt
-start index.html
-python -m uvicorn server:app --reload --port 8000
-pause
